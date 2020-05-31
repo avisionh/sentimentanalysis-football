3d9f269dceeefa43725d455a606190f1a64c5422
avisionh <a_vision@hotmail.co.uk>
2020-05-31 22:48:32 +0100

    docs: Add comments for cleaning
    This is to describe thinking process for why we are wrangling our data in a specific way.
    
    Also provided links to sites we will use.

--- notebook.py
+++ notebook.py
@@ -5,6 +5,11 @@
 # Using `textblob` library
 # 
 # Data comes from Kaggle dataset of football events from [Alin Secareanu](https://www.kaggle.com/secareanualin/football-events).
+# 
+# Following the below tutorials:
+# 
+# - [Python programming](https://pythonprogramming.net/sentiment-analysis-python-textblob-vader/)
+# - [DigitalOcean](https://www.digitalocean.com/community/tutorials/how-to-perform-sentiment-analysis-in-python-3-using-the-natural-language-toolkit-nltk)
 
 import pandas as pd
 from textblob import TextBlob
@@ -31,6 +36,32 @@ data_commentary.info()
 
 data_commentary['text'] = data_commentary['text'].astype(str)
 
+
+# Now, we could analyse the sentiment of each individual line of commentary but thinking about what we are trying to achieve, namely:
+# 
+# > Analyse the sentiment associated to teams for **each game** so that we have a proxy for how good the team is
+# 
+# Then given this aim, what we want to do is *squash* **all** the commentary associated to the home and away side in a game and analyse the overall sentiment for each side from that. We then have sentiments attached to each team for each game and by deciding the sentiments overall, we can gauge how well the team performed in all their games within the dataset and henceforth, how good the team are. 
+# 
+# For instance, consider Borussia Dortmund. If the overall sentiment from each of their games played was positive, and most of their games had a positive sentiment, then this suggests Borussia Dortmund are a good team, which as a football fan, we know is true. 
+# 
+# ### Assumption
+# Within this framework, we make the following assumptions:
+# 
+# - Commentary recorded in the dataset reports on all main events in a game.
+# - Commentary recorded in the dataset will report the good and bad events associated to the home and away sides in each game.
+#     + This means the commentary is an accurate reflection of all the major events in each game.
+
+# ***
+# 
+# ## Baseline
+# In this section, we will apply the `.sentiment` method on our commentary so that we can get a baseline of results that we can then compare future outputs against. In particular, the idea is that we apply this `sentiment` method on the unprocessed data. Then we process the data by doing things like:
+# 
+# - Lemmatise
+# - Stem
+# - ...
+# 
+# After processing the data, we should get a better dataset to start extracting the sentiments from.
 
 # inspect sentiment of each row
 for row_line in data_commentary['text']:
