0f899d851352d2e50b3477e60e7b0f31ae023df2
avisionh <a_vision@hotmail.co.uk>
2020-05-31 22:33:37 +0100

    feat: Analyse sentiment of each text commentary
    This is to provide a baseline to compare against.

--- notebook.py
+++ notebook.py
@@ -20,9 +20,27 @@ data_meta = pd.read_csv(filepath_or_buffer = 'data/dictionary.txt', sep = '\t')
 
 # view data
 data_commentary.head(10)
-data_meta.head(20)
+data_meta.head(15)
+
+
+# check info of data types
+data_commentary.info()
+
+
+# Need to convert object of interest into a `TextBlob` object so we can use things from `TextBlob` package.
+
+data_commentary['text'] = data_commentary['text'].astype(str)
+
+
+# inspect sentiment of each row
+for row_line in data_commentary['text']:
+    row_analyse = TextBlob(row_line)
+    row_analyse.sentiment
 
 
 # output as Python script for nice version-controlling
-get_ipython().system('jupyter nbonvert --to script notebook.ipynb')
+get_ipython().system('jupyter nbconvert --to script --no-prompt notebook.ipynb')
+
+
+
 
